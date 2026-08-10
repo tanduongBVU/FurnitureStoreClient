7f85f5450b84f6472588a9d0cef0be307b18bf13
Nguyen Tan Duong <duongnguyentan800@gmail.com>
2026-08-10 15:41:50 +0700

    depoy lần 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7025/api",
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("clientToken");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
